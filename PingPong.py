9d7aa0801724d6fa96e86a0c84f9106d88f57656
afumofumo <luisalonso180510@gmail.com>
2025-09-29 19:56:32 -0600

    Add files via upload

--- PingPong.py
+++ PingPong.py
@@ -12,10 +12,12 @@ window.fill(back)
 clock = time.Clock()
 FPS = 60
 
+game = True
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.loaf(player_image), (wight, height))
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -37,14 +39,15 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-game = True
 
-
+ball = GameSprite("ball.png", 200, 200, 4, 50, 50)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
         
+        ball.reset()
+
     display.update()
     clock.tick(FPS)
